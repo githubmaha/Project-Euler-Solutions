8de87188b3f7c74dbeaf8d2b8d88c3a5296dcc78
Mahamitra Jagadheshkumar <mahamitraDJ@gmail.com>
2020-08-18 18:49:20 -0400

    Problem 1 java solution

---
+++ Problem1.java
@@ -0,0 +1,9 @@
+class Problem1 {
+    public static void main(String[] args) {
+        int sum3 = 3 * 333 * 334 / 2;
+        int sum5 = 5 * 199 * 200 / 2;
+        int sum15 = 15 * 66 * 67 / 2;
+
+        System.out.println(sum3 + sum5 - sum15);
+    }
+}
